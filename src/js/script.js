55a110aa339cf026c15df7b06ab441e71d6b5122
Katerina Ushakova <lechatkik@gmail.com>
2020-03-02 16:42:24 +0300

    Create a carousel witn jquery

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  $('.owl-carousel').owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoHeight: true
+});
