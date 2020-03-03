e865ae8975830e48aa4de4068c5b2dc7aada84f1
Katerina Ushakova <lechatkik@gmail.com>
2020-03-03 19:17:44 +0300

    Finished menu catalog, js

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,22 @@ $(document).ready(function(){
     nav:true,
     autoHeight: true
 });
+
+$('ul.tabs').on('click', 'li:not(.tab_active)', function() {
+  $(this)
+    .addClass('tab_active').siblings().removeClass('tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+function toggleSlide (item) {
+  $(item).each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__description').eq(i).toggleClass('catalog-item__description_active');
+    })
+  });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
